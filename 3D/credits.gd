c65c857153c216082cd3ddfd0a4560d42fe99acf
SW1073 <eloiimerino@gmail.com>
2023-12-29 14:48:20 +0100

    [ADD] Music and MusicController autoload

--- 3D/credits.gd
+++ 3D/credits.gd
@@ -1,4 +1,7 @@
 extends Control
 
+func _ready():
+	print("pa ti mi cola")
+
 func _on_back_button_up():
 	get_tree().change_scene_to_file("res://menu.tscn")
--- 3D/menu.gd
+++ 3D/menu.gd
@@ -1,16 +1,13 @@
 extends Control
 
+func _ready():
+	$"/root/MusicController".play_menu_music()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
 	pass
-	
+
 func _on_play_button_up() -> void:
+	$"/root/MusicController".play_level_music()
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_quit_button_up() -> void:
