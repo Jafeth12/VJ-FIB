f9c5e306b322020304f9c65d2c7474391589f338
jafeth <jafethrafael12@gmail.com>
2024-01-01 11:58:39 +0100

    [IMP] can open chest

--- 3D/levels/chest.gd
+++ 3D/levels/chest.gd
@@ -3,21 +3,31 @@ extends StaticBody3D
 signal player_entered_area
 signal player_exited_area
 
+var can_be_opened: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
+func can_open() -> bool:
+	return can_be_opened
+
+func open():
+	print("Chest opened")
+
+# ------------------------------
 
 func _on_body_entered(body):
 	if body is Player:
 		emit_signal("player_entered_area")
-
+		can_be_opened = true
 
 func _on_body_exited(body):
 	if body is Player:
 		emit_signal("player_exited_area")
+		can_be_opened = false
--- 3D/levels/level1.gd
+++ 3D/levels/level1.gd
@@ -1,22 +1,28 @@
 extends Node
 
-@onready var player = get_node("root/main/Player")
+@onready var player = get_node("/root/main/Player")
+@onready var platforms = get_tree().get_nodes_in_group("lvl1_platforms")
+@onready var chests = get_tree().get_nodes_in_group("lvl1_chests")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var platforms = get_tree().get_nodes_in_group("lvl1_platforms")
 	for platform in platforms:
 		platform.connect("player_entered_platform", on_player_entered_platform)
 		platform.connect("player_exited_platform", on_player_exited_platform)
 		
-	var chests = get_tree().get_nodes_in_group("lvl1_chests")
 	for chest in chests:
 		chest.connect("player_entered_area", _on_chest_player_entered_area)
 		chest.connect("player_exited_area", _on_chest_player_exited_area)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	handle_input()
+	
+func handle_input() -> void:
+	if Input.is_action_just_pressed("interact"):
+		for chest in chests:
+			if chest.can_open():
+				chest.open()
 
 func on_player_entered_platform() -> void:
 	player.player_set_on_platform(true)
