80f64288fa71845ed94e25d4baf79946587585a5
SW1073 <eloiimerino@gmail.com>
2024-01-05 11:57:08 +0100

    [REF] separación de la lógica de decisión de color a función separada

--- 3D/ui/hud/HUD.gd
+++ 3D/ui/hud/HUD.gd
@@ -17,14 +17,17 @@ func format_time(seconds: float) -> String:
 	secs = str(snapped(fmod(seconds, 60.0), 0.01)).pad_zeros(2)
 	return  mins + secs
 
+func timer_color(seconds: float) -> Color:
+	if seconds < 30.0:
+		return Color.RED
+	else:
+		return Color.WHITE
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var time_left = MainLogic.get_game_time_left()
-	if time_left < 30.0:
-		$timer.modulate = Color(1, 0, 0, 1)
-	else:
-		$timer.modulate = Color(1, 1, 1, 1)
-	$timer.text = format_time(time_left)
+	var seconds_left = MainLogic.get_game_time_left()
+	$timer.modulate = timer_color(seconds_left)
+	$timer.text = format_time(seconds_left)
 
 # ------------------------------
 
