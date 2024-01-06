a8f75c5206bc65c9db2622e1c0ce22826e952b91
SW1073 <eloiimerino@gmail.com>
2024-01-06 02:06:13 +0100

    [FIX] botones de debug casi todos fuera

--- 3D/entities/player/player.gd
+++ 3D/entities/player/player.gd
@@ -253,11 +253,7 @@ func player_move_dir() -> EntityDirection:
 func player_handle_input() -> void:
 	if Input.is_action_just_pressed("god_mode"):
 		god_mode = !god_mode
-	if Input.is_action_just_pressed("dbg_die"):
-		health = 0
-	if Input.is_action_just_pressed("dbg_take_damage"):
-		player_take_damage(20)
-	if Input.is_action_just_pressed("dbg_switch_ring") || (is_on_platform && Input.is_action_just_pressed("interact")):
+	if (is_on_platform && Input.is_action_just_pressed("interact")):
 		player_change_ring_state()
 	if Input.is_action_just_pressed("dbg_next_level") || (can_go_to_next_height && Input.is_action_just_pressed("change_level_height")):
 		if resetting_alpha:
